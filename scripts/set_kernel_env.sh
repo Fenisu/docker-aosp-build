0fb1c4afc8f56c4db98f8e4245f07ee9929f315b
Ignacio Quezada <dreamtrick@gmail.com>
2016-07-07 14:27:33 +0200

    Added bootimg to the kernel env

--- scripts/set_kernel_env.sh
+++ scripts/set_kernel_env.sh
@@ -14,14 +14,57 @@ main(){
   for gcc_index in "${!gcc_tools[@]}"; do
     echo "- [$gcc_index] ${gcc_tools[$gcc_index]}"
   done
-  echo -n [*] What version do you want to use? [0-${gcc_index}] \ 
-  read answer
-  echo "[*] Now run:"
-  echo "  source /kernel/.kernel.env"
+  echo -n "[*] What version do you want to use? [0] " 
+  read gccversion
+  if [[ $gccversion == "" ]]; then
+    answer=0
+  fi
   echo "export CC=${gcc_tools[$answer]%gcc}" > /kernel/.kernel.env
   echo "export CROSS_COMPILE=${gcc_tools[$answer]%gcc}" >> /kernel/.kernel.env
   echo "export ARCH=arm" >> /kernel/.kernel.env
   echo "export SUBARCH=arm" >> /kernel/.kernel.env
+  echo -n "[*] Do you need to install bootimg-tools? [y/N] "
+  read bitools
+  if [[ $bitools == "y" || $bitools == "Y" ]]; then
+    bootimg
+    if [[ $? -eq 0 ]]; then
+      echo "alias mkbootimg=/tools/bootimg-tools/mkbootimg/mkbootimg" >> /kernel/.kernel.env
+      echo "alias unmkbootimg=/tools/bootimg-tools/mkbootimg/unmkbootimg" >> /kernel/.kernel.env
+    fi
+  fi
+  echo -n "[*] Is it already installed? [Y/n] "
+  read bitools
+  if [[ $bitools == "Y" || $bitools == "y" || $bitools == "" ]]; then
+    mkb=$(find /tools -type f -name 'mkbootimg' -print0)
+    unmkb=$(find /tools -type f -name 'unmkbootimg' -print0)
+    if [[ $mkb != "" && $unmkb != "" ]]; then
+      echo "alias mkbootimg=${mkb}" >> /kernel/.kernel.env
+      echo "alias unmkbootimg=${unmkb}" >> /kernel/.kernel.env
+    fi
+  fi
+  echo "[*] Now run:"
+  echo "  . /kernel/.kernel.env"
+}
+
+bootimg(){
+  echo "[*] Cloning bootimg-tools into /tools"
+  git clone https://github.com/pbatard/bootimg-tools.git /tools/bootimg-tools
+  if [[ $? -ne 0 ]]; then
+    echo "[-] Source retrieval failed."
+    return 1
+  fi
+  echo "[*] Compiling bootimg-tools.."
+  cd /tools/bootimg-tools
+  make -j 2
+  if [[ $? -ne 0 ]]; then
+    echo "[-] Compilation failed."
+    return 1
+  else
+    echo "[*] bootimg-tools ready to use."
+  fi
+  return 0
 }
 
 main
+
+exit 0
